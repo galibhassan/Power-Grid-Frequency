8da36e5009081d8a7b7af796c4efd712b2283bac
Galib Hassan <galib.hassan.0@gmail.com>
2020-04-23 02:23:30 +0200

    wip

--- treeData.js
+++ treeData.js
@@ -23,4 +23,52 @@ const treeData = [
       },
     ],
   },
+  {
+    name: "France",
+    type: "folder",
+    children: [
+      {
+        name: "2019",
+        type: "folder",
+        children: [
+          { name: "January", type: "file", downloadURL: "www.google.com" },
+          { name: "February", type: "file", downloadURL: "www.google.com" },
+          { name: "March", type: "file", downloadURL: "www.google.com" },
+        ],
+      },
+      {
+        name: "2017",
+        type: "folder",
+        children: [
+          { name: "January", type: "file", downloadURL: "https://www.prothomalo.com/" },
+          { name: "February", type: "file", downloadURL: "https://www.facebook.com/" },
+          { name: "March", type: "file", downloadURL: "https://github.com/" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Turkey",
+    type: "folder",
+    children: [
+      {
+        name: "2019",
+        type: "folder",
+        children: [
+          { name: "January", type: "file", downloadURL: "www.google.com" },
+          { name: "February", type: "file", downloadURL: "www.google.com" },
+          { name: "March", type: "file", downloadURL: "www.google.com" },
+        ],
+      },
+      {
+        name: "2017",
+        type: "folder",
+        children: [
+          { name: "January", type: "file", downloadURL: "https://www.prothomalo.com/" },
+          { name: "February", type: "file", downloadURL: "https://www.facebook.com/" },
+          { name: "March", type: "file", downloadURL: "https://github.com/" },
+        ],
+      },
+    ],
+  },
 ];
