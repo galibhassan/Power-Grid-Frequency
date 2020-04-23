c208da0c27e1116ac2c8aaf6b5df12685e7adc47
Galib Hassan <galib.hassan.0@gmail.com>
2020-04-23 18:09:45 +0200

    include germany 2011 data

--- doc_new/treeData.js
+++ doc_new/treeData.js
@@ -4,56 +4,118 @@ const treeData = [
     type: "folder",
     children: [
       {
-        name: "2019",
+        name: 2011,
         type: "folder",
         children: [
-          { name: "January", type: "file", downloadURL: "www.google.com" },
-          { name: "February", type: "file", downloadURL: "www.google.com" },
-          { name: "March", type: "file", downloadURL: "www.google.com" },
+          {
+            name: "July",
+            type: "folder",
+            children: [
+              {
+                name: "Data",
+                type: "file",
+                downloadURL:
+                  "https://github.com/LRydin/Power-Grid-Frequency-Data/blob/master/Continental%20Europe/Germany/2011/07/2011_07.csv.zip?raw=true",
+              },
+              {
+                name: "Plot",
+                type: "file",
+                downloadURL:
+                  "https://raw.githubusercontent.com/LRydin/Power-Grid-Frequency-Data/master/Continental%20Europe/Germany/2011/07/2011_07.png",
+              },
+            ],
+          },
+          {
+            name: "August",
+            type: "folder",
+            children: [
+              {
+                name: "Data",
+                type: "file",
+                downloadURL:
+                  "https://github.com/LRydin/Power-Grid-Frequency-Data/blob/master/Continental%20Europe/Germany/2011/08/2011_08.csv.zip?raw=true",
+              },
+              {
+                name: "Plot",
+                type: "file",
+                downloadURL:
+                  "https://raw.githubusercontent.com/LRydin/Power-Grid-Frequency-Data/master/Continental%20Europe/Germany/2011/08/2011_08.png",
+              },
+            ],
+          },
+          {
+            name: "September",
+            type: "folder",
+            children: [
+              {
+                name: "Data",
+                type: "file",
+                downloadURL:
+                  "https://github.com/LRydin/Power-Grid-Frequency-Data/blob/master/Continental%20Europe/Germany/2011/09/2011_09.csv.zip?raw=true",
+              },
+              {
+                name: "Plot",
+                type: "file",
+                downloadURL:
+                  "https://raw.githubusercontent.com/LRydin/Power-Grid-Frequency-Data/master/Continental%20Europe/Germany/2011/09/2011_09.png",
+              },
+            ],
+          },
+          {
+            name: "October",
+            type: "folder",
+            children: [
+              {
+                name: "Data",
+                type: "file",
+                downloadURL:
+                  "https://github.com/LRydin/Power-Grid-Frequency-Data/blob/master/Continental%20Europe/Germany/2011/10/2011_10.csv.zip?raw=true",
+              },
+              {
+                name: "Plot",
+                type: "file",
+                downloadURL:
+                  "https://raw.githubusercontent.com/LRydin/Power-Grid-Frequency-Data/master/Continental%20Europe/Germany/2011/10/2011_10.png",
+              },
+            ],
+          },
+          {
+            name: "November",
+            type: "folder",
+            children: [
+              {
+                name: "Data",
+                type: "file",
+                downloadURL:
+                  "https://github.com/LRydin/Power-Grid-Frequency-Data/blob/master/Continental%20Europe/Germany/2011/11/2011_11.csv.zip?raw=true",
+              },
+              {
+                name: "Plot",
+                type: "file",
+                downloadURL:
+                  "https://raw.githubusercontent.com/LRydin/Power-Grid-Frequency-Data/master/Continental%20Europe/Germany/2011/11/2011_11.png",
+              },
+            ],
+          },
+          {
+            name: "December",
+            type: "folder",
+            children: [
+              {
+                name: "Data",
+                type: "file",
+                downloadURL:
+                  "https://github.com/LRydin/Power-Grid-Frequency-Data/blob/master/Continental%20Europe/Germany/2011/12/2011_12.csv.zip?raw=true",
+              },
+              {
+                name: "Plot",
+                type: "file",
+                downloadURL:
+                  "https://raw.githubusercontent.com/LRydin/Power-Grid-Frequency-Data/master/Continental%20Europe/Germany/2011/12/2011_12.png",
+              },
+            ],
+          },
         ],
-      },
-      {
-        name: "2017",
-        type: "folder",
-        children: [
-          { name: "January", type: "file", downloadURL: "https://www.prothomalo.com/" },
-          { name: "February", type: "file", downloadURL: "https://www.facebook.com/" },
-          { name: "March", type: "file", downloadURL: "https://github.com/" },
-        ],
-      },
-    ],
-  },
-  {
-    name: "France",
-    type: "folder",
-    children: [
-      {
-        name: "2019",
-        type: "folder",
-        children: [
-          { name: "January", type: "file", downloadURL: "www.google.com" },
-          { name: "February", type: "file", downloadURL: "www.google.com" },
-          { name: "March", type: "file", downloadURL: "www.google.com" },
-        ],
-      },
-      {
-        name: "2017",
-        type: "folder",
-        children: [
-          { name: "January", type: "file", downloadURL: "https://www.prothomalo.com/" },
-          { name: "February", type: "file", downloadURL: "https://www.facebook.com/" },
-          { name: "March", type: "file", downloadURL: "https://github.com/" },
-        ],
-      },
-    ],
-  },
-  {
-    name: "Turkey",
-    type: "folder",
-    children: [
-      {
-        name: "still to be uploaded",
-        type: "file",
       },
     ],
   },
